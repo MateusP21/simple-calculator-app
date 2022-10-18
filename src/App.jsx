6556604c2d1015a7c97537e6768c979d1c9241ec
MateusP21 <mateuspasso@outlook.com>
2022-10-17 22:47:54 -0300

    feat: aplicando o darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { DarkModeToggle } from 'react-dark-mode-toggle-2';
 function App() {
   const [currentOperator, setCurrentOperator] = useState(null);
   const [calculationValue, setCalculationValue] = useState('');
@@ -7,12 +8,20 @@ function App() {
     secondValue: '',
   });
 
+  const [colorTheme, setColorTheme] = useState('light');
+
   const operations = {
     '+': (first, second) => first + second,
     '-': (first, second) => first - second,
     '/': (first, second) => first / second,
     X: (first, second) => first * second,
   };
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.removeAttribute('class');
+    root.classList.add(colorTheme);
+  }, [colorTheme]);
 
   useEffect(() => {
     if (currentOperator === null) {
@@ -63,9 +72,17 @@ function App() {
     setCalculator((prevState) => ({ ...prevState, firstValue: result }));
   };
   return (
-    <main className="container mx-auto p-4 flex items-center justify-center h-screen">
-      <section className="border rounded shadow-md bg-white max-w-xs">
-        <div className="p-4 py-6 text-right border-b">
+    <main className=" dark:bg-slate-900 mx-auto p-4 flex items-center justify-center h-screen">
+      <section className="border dark:border-slate-600 flex flex-col rounded shadow-md bg-white dark:bg-slate-900 max-w-xs">
+        <DarkModeToggle
+          className="self-end mt-4 mr-4"
+          size={50}
+          isDarkMode={colorTheme === 'dark' ? true : false}
+          onChange={() =>
+            setColorTheme(colorTheme === 'light' ? 'dark' : 'light')
+          }
+        />
+        <div className="p-4 py-6 text-right border-b dark:border-slate-600">
           <p className="text-sm text-gray-400 pb-4">
             {`${calculator.firstValue === '' ? '0' : calculator.firstValue} ${
               currentOperator === null ? '' : currentOperator
@@ -76,7 +93,7 @@ function App() {
               placeholder="0"
               disabled
               value={calculationValue}
-              className="text-7xl overflow-hidden bg-transparent placeholder:text-right text-right"
+              className="text-7xl overflow-hidden dark:text-slate-50 bg-transparent placeholder:text-right text-right"
             ></input>
           </div>
         </div>
